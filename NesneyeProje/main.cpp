f5a3f47e8ce8c0edf8124ebbc071206569ce1eef
anas <b210109577@subu.edu.tr>
2025-01-04 16:29:57 +0300

    SavasOyunu sinifi icin oyunuBaslat fonksiyonu tanimlandi

--- NesneyeProje/main.cpp
+++ NesneyeProje/main.cpp
@@ -17,3 +17,48 @@ public:
         delete kullanici;
         delete dusman;
     }
+
+    void oyunuBaslat() {
+        cout << "=== Savas Basliyor! ===" << endl;
+        while (kullanici->hayattaMi() && dusman->hayattaMi()) {
+            cout << "\n";
+            durumGoster();
+
+            int secim = 0;
+            while (true) {
+                cout << "1. Saldir\n2. Savun\n3. Ozel Yetenek\nSeciminiz: ";
+                cin >> secim;
+
+                if (secim == 1 || secim == 2 || secim == 3) {
+                    break;
+                } else {
+                    cout << "Gecersiz secim! Lutfen tekrar deneyin." << endl;
+                }
+            }
+
+            if (secim == 1) {
+                kullanici->saldir(dusman);
+            } else if (secim == 2) {
+                kullanici->savun();
+            } else if (secim == 3) {
+                kullanici->ozelYetenek();
+            }
+
+            dusman->sifirlaSavunma();
+
+            if (dusman->hayattaMi()) {
+                int dusmanSecim = rand() % 3 + 1;
+                if (dusmanSecim == 1) {
+                    dusman->saldir(kullanici);
+                } else if (dusmanSecim == 2) {
+                    dusman->savun();
+                } else {
+                    dusman->ozelYetenek();
+                }
+            }
+
+                    kullanici->sifirlaSavunma();
+        }
+
+        sonucuGoster();
+    }
