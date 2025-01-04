8b0b6e89537de1b99918a096a84d389e3b7a189d
anas <b210109577@subu.edu.tr>
2025-01-04 16:25:22 +0300

    SavasOyunu sinifi olusturuldu ve temel constructor ile destructor tanimlandi

--- NesneyeProje/main.cpp
+++ NesneyeProje/main.cpp
@@ -1,3 +1,19 @@
 #include "Karakter.h"
 #include <cstdlib>
 #include <ctime>
+
+class SavasOyunu {
+private:
+    Kullanici* kullanici;
+    Dusman* dusman;
+
+public:
+    SavasOyunu(const string& kullaniciIsim)
+        : kullanici(new Kullanici(kullaniciIsim)), dusman(new Dusman("Dusman")) {
+        srand(static_cast<unsigned>(time(0)));
+    }
+
+    ~SavasOyunu() {
+        delete kullanici;
+        delete dusman;
+    }
